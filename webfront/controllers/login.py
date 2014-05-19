fbb83edf8d4b1d8dc410b0a79a0a8e6c8e050029
Cody Harrington <thecodeman66@hotmail.com>
2014-05-20 03:14:34 +1200

    Added libraries and got a simple design going for the login page

--- webfront/controllers/login.py
+++ webfront/controllers/login.py
@@ -1,3 +1,12 @@
 __author__ = 'cody'
 from webfront import app
+from flask import render_template, request
 
+@app.route("/login", methods=["GET"])
+def load_login_page():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def process_login():
+    print(request.args)
+    return "Ye"
