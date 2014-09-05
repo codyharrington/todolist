16e64c211c3f71a05b54f15824525672b9c83545
Cody Harrington <thecodeman66@hotmail.com>
2014-09-05 18:58:28 +1200

    Updated the test

--- dbapi/test/task_test.py
+++ dbapi/test/task_test.py
@@ -1,12 +1,14 @@
 __author__ = 'cody'
 
 from dbapi.test.testing_utils import *
+import json
 
 class TaskTest(DbapiTestCase):
 
     def test_get_task(self):
         return_value = self.app.get("/task/{}".format(self.test_task.id))
-        print(return_value)
+        self.assertEqual(self.test_task.todict(), json.loads(return_value.data.decode())["data"])
+
 
 if __name__ == '__main__':
     unittest.main()
